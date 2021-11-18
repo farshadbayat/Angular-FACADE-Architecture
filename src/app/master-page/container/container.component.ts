48f19788e2efc9fbb2df8392a4a066ee0b299400
Farshad Bayat <farshad.bayat@gmail.com>
2021-11-18 13:34:35 +0330

    َیی ,سثق ّثفزا

--- src/app/master-page/container/container.component.ts
+++ src/app/master-page/container/container.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '@core/services/global.service';
+import { ApiRequest } from '@core/services/request.service';
 
 @Component({
   selector: 'app-container',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gs: GlobalService) { }
 
   ngOnInit(): void {
+    this.fetchUser();
+  }
+
+  fetchUser() {
+    const url = 'https://customergateway.bidballer.com/test/';
+    const request = ApiRequest('GET', false).setBaseURL(url).setModuleName('performance')
+    .setController('bot').setAction('userlist');
+    request.addParam('Count', 4)
+    .addParam('SaleID', 7788);
+    this.gs.apiRequest<any>(request).subscribe( resp => {
+      debugger
+      if ( resp.Success === true) {
+      }
+    });
   }
 
 }
